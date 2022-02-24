4f1d20edf03ee414ed1beafa5d886c6163577d07
Prasanna <Prasanna>
2022-02-24 18:38:38 +0530

    otehr changes

--- lisp/custom.el
+++ lisp/custom.el
@@ -15,6 +15,7 @@
 (tooltip-mode 1)
 (ivy-rich-mode 1)
 (mood-line-mode 1)
+(all-the-icons-ivy-rich-mode 1)
 ;; (auto-dim-other-buffers-mode 1)
 
 (setq file-name-handler-alist nil)
@@ -82,6 +83,7 @@
 
 ;; (set-face-attribute 'hl-line nil :background "#454545")
 
+
 (let ((alist
        `((?! . ,(regexp-opt '("!!" "!=" "!==")))
          (?# .
@@ -124,7 +126,6 @@
 
 (setq warning-minimum-level :emergency)
 
-
 ;; 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -143,10 +144,12 @@
  '(markdown-link-face ((t (:background "#0e1014" :foreground "#bd93f9"))))
  '(markdown-list-face ((t (:foreground "mediumpurple"))))
  '(markdown-pre-face ((t (:foreground "#bd98fe"))))
- )
+ '(mc/cursor-bar-face ((t (:height 1 :foreground "white"))))
+ '(mc/cursor-face ((t (:foreground "white" )))))
 
-;; (load-theme 'modus-vivendi t)
-(load-theme 'vscode-dark-plus t)
+(load-theme 'modus-vivendi t)
+;; (load-theme 'vscode-dark-plus t)
+
 
 ;; (set-face-attribute 'default nil :family "PragmataPro Liga" :height 190)
 ;; (set-face-attribute 'fixed-pitch nil :family "PragmataPro Liga" :height 190)
@@ -163,26 +166,19 @@
       (let ((currbuff (current-buffer)))
         (shell)
         (switch-to-buffer currbuff)
-        (setq proc (get-process "shell"))
-        ))
+        (setq proc (get-process "shell"))))
     (setq pbuff (process-buffer proc))
     (if (use-region-p)
-        (setq min (region-beginning)
-              max (region-end))
-      (setq min (point-at-bol)
-            max (point-at-eol)))
+        (setq min (region-beginning) max (region-end))
+      (setq min (point-at-bol) max (point-at-eol)))
     (setq command (concat (buffer-substring min max) "\n"))
     (with-current-buffer pbuff
       (goto-char (process-mark proc))
       (insert command)
-      (move-marker (process-mark proc) (point))
-      ) ;;pop-to-buffer does not work with save-current-buffer -- bug?
+      (move-marker (process-mark proc) (point))) ;;pop-to-buffer does not work with save-current-buffer -- bug?
     (process-send-string  proc command)
     (display-buffer (process-buffer proc) t)
-    (when step 
-      (goto-char max)
-      (next-line))
-    ))
+    (when step (goto-char max) (next-line))))
 
 (defun sh-send-line-or-region-and-step ()
   (interactive)
